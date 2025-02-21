d79c3fb4001a5740e5aea3e0a7e28111d8d8e3db
zjykzj <wy163zhuj@163.com>
2025-02-21 14:28:27 +0800

    refactor(yolo8face): remove yolo8face, update to yolo11face

--- yolo8face/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""
-@Time    : 2025/1/19 15:19
-@File    : __init__.py
-@Author  : zj
-@Description: 
-"""
--- yolo8face/cfg/datasets/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""
-@Time    : 2025/1/19 15:20
-@File    : __init__.py.py
-@Author  : zj
-@Description: 
-"""
